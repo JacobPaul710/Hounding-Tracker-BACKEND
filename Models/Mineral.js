5f7bee3b0bbc7d997ada12b38465c6790c5bf94d
JacobPaul710 <jacobpaulscott@outlook.com>
2023-05-26 19:53:00 -0700

    Updates MineralSchema.

--- Models/Mineral.js
+++ Models/Mineral.js
@@ -2,26 +2,30 @@ const mongoose = require('../config/connection');
 
 const MineralSchema = new mongoose.Schema(
     {
+    mineralType: {
+        type: String,
+        required: [true, 'Type of mineral found is required (ex. Carnelian).']
+    },
     date: {
          type: Date,
-         required: true
+         required: [true, 'Date mineral was found is required.']
     },
     conditions: {
         type: String,
-         required: true
+         required: [true, 'Conditions mineral was found in required']
     },
     county: {
         type: String,
-        required: true
+        required: [true, 'County mineral was found in required']
    },
     image: 
     {
         type: String,
-        required: true
+        required: [true, 'Image of mineral required']
    },
     houndingName: {
             type: String,
-            required: true,
+            required: [true, 'Name of hounder (can be username or real).'],
             unique: true
        }, 
     },
