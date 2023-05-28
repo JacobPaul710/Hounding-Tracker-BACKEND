4ef4b8b01b060de82a6a12bddba7b976305277d3
JacobPaul710 <jacobpaulscott@outlook.com>
2023-05-28 15:02:01 -0700

    Creates functioning Seed Route.

--- controllers/minerals.js
+++ controllers/minerals.js
@@ -2,11 +2,48 @@ const express = require('express');
 const Mineral = require('../Models/Mineral');
 const router = express.Router();
 
+const mineralSeed = [
+    {
+        "mineralType": "Carnelian",
+        "date": "09-03-22",
+        "conditions": "Sunny",
+        "county": "Linn",
+        "image": "https://minerals.net/GemStoneInTheRoughImages/carnelian-rough-oregon.jpg",
+        "houndingName": "JacobPaul710"
+    },
+    {
+        "mineralType": "Holley Blue",
+        "date": "04-20-00",
+        "conditions": "Cloudy",
+        "county": "Linn",
+        "image": "https://imgur.com/a/cDuvnvD",
+        "houndingName": "JacobPaul7101"
+    },
+    {
+        "mineralType": "Limb Cast",
+        "date": "07-10-00",
+        "conditions": "Cloudy",
+        "county": "Deschutes County",
+        "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRtxoxjpLfXdXlcBV02PiDNv3-FSBKhbqSlxQ&usqp=CAU",
+        "houndingName": "JacobPaul7102"
+    }
+]
+
 router.get('/', async(req, res) => {
     try{
         res.json(await Mineral.find({}))
     } catch(error){
         console.log("GET Request Error:", error)
+    }
+})
+
+router.get('/seed', async (req, res) => {
+    try {
+        await Mineral.deleteMany({});
+        await Mineral.insertMany(mineralSeed);
+        res.redirect('/minerals');
+    } catch(error) {
+        console.log("SEED Route Error", error);
     }
 })
 
